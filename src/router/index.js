e93d2cde222d619852112b01c6ed398935aaaabc
Dean Lightning <thedeanlightning@gmail.com>
2021-07-13 09:54:41 +0800

    Update router mode

--- src/router/index.js
+++ src/router/index.js
@@ -21,9 +21,12 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
 
 export default router
